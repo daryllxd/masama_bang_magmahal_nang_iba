beabdd072214d24cf9ec2c27b796b4191df8d8d9
Daryll Santos <daryll.santos@gmail.com>
2014-04-26 20:36:48 +0800

    prepare to install bower

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+//= require jquery
+//= require eqeqdwq
+//
+
+  // -# <script src="public/vendor/jquery/jquery.min.js"></script>
+  // -# <script src="public/js/gmaps.js"></script>
+  // -# <script src="public/vendor/queryloader/jquery.queryloader2.min.js"></script>
+  // -# <script src="public/vendor/jquery/jquery.sticky.js"></script>
+  // -# <script src="public/js/stackblur.js"></script>
+  // -# <script src="public/js/app.js"></script>
+// -# %script{:src => "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"}
